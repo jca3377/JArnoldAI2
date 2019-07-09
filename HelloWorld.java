0f55ac99df6f57221e9c2a11d2730317b89a3e80
Julia Arnold <jca3377@gmail.com>
2019-07-09 10:01:49 -0400

    AI2 Day 1 exercises

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { //Start of the program
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+	}
+}
---
+++ MyName.java
@@ -0,0 +1,7 @@
+public class MyName {
+	public static void main(String[] args) {
+		System.out.print("Julia ");
+		System.out.print("Arnold ");
+		System.out.println("grade 10");
+	}
+}
